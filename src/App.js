348ddc17e0eb94ef25e2856a5ab4fb13b2d59aa3
Chay Williams <Chayvw@gmail.com>
2021-05-20 15:30:27 -0400

    installed npm packages axios and react router dom and made state and statless componets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from '../src/containers/Welcome';
+import Main from './componets/Main';
+import Form from './componets/Form';
+import Results from './componets/Results';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h3>App</h3>
+     <Router>
+       <Switch>
+       <Route exact path='/' component={Welcome} />
+       <Route path='/main' component={Main} />
+       <Route path='/form' component={Form} />
+       <Route path='/results' component={Results} />
+       </Switch>
+     </Router>
+
     </div>
   );
 }
---
+++ src/componets/Results.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Results(props) {
+    return (
+        <div>
+            <h5>Results</h5>
+        </div>
+    );
+}
+
+export default Results;
